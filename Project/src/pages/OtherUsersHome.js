451f7e1dc2215294a7fb7bb7172c193859ca2ced
Fr4nk03 <jx37@illinois.edu>
2023-03-03 23:57:01 -0600

    Updates
    Updates

---
+++ Project/src/pages/OtherUsersHome.js
@@ -0,0 +1,11 @@
+// Home Page viewed by other users
+import React from 'react';
+const OtherUsersHome = () => {
+  return (
+    <div>
+      <h1>This is the user's question Box! Feel free to ask!</h1>
+    </div>
+  );
+};
+  
+export default OtherUsersHome;
