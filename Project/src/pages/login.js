6b24fe1ee5fffb132f3e8c60944cef3e0f2a53ef
Ding-jinyu <jinyud2@illinois.edu>
2023-03-08 16:41:17 -0600

    worked on login page's css

--- Project/src/pages/login.js
+++ Project/src/pages/login.js
@@ -11,23 +11,22 @@ const Login = () => {
     console.log(email);
   }
   return (
-    <div style={{backgroundColor:"#FFFFFF"}}><h1>Nice to see u!</h1>
-    <div style={{textAlign:"center", display:"flex", minHeight: "100vh",
-        alignItems: "center", justifyContent: "center", backgroundColor:"#F7944D"}}>
+    <div className= "login">
+    {/* <div style={{textAlign:"center", display:"flex", minHeight: "100vh",
+        alignItems: "center", justifyContent: "center", backgroundColor:"#F7944D"}}> */}
     
-    <form onSubmit={handleSubmit}>
-      {/* email */}
-      <label for="email">email</label>
-      <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='your email' id="email" name="email" />
-      {/* password */}
-      <label for="password">password</label>
-      <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='your password' id="password" name="password" />
-      {/* button */}
-      <button style={{border: "none", backgroundColor:"white", padding:20, borderRadius:10, cursor:"pointer", color:"#7439da" }} 
-      type="submit"> Log In </button>
-    </form>
+      <form className = "login-form" onSubmit={handleSubmit}>
+        {/* email */}
+        <label for="email">email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='your email' id="email" name="email" />
+        {/* password */}
+        <label for="password">password</label>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='your password' id="password" name="password" />
+        {/* button */}
+      <button> Log In </button>
+      </form>
     </div>
-    </div>
+    // </div>
   );
 };
 export default Login;
