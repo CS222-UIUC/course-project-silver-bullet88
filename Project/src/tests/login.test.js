04546e7a59fc6ab8d4f2fd969821259470e619c3
Ding-jinyu <jinyud2@illinois.edu>
2023-03-08 18:59:07 -0600

    again

--- Project/src/tests/login.test.js
+++ Project/src/tests/login.test.js
@@ -9,8 +9,7 @@ test('renders login page', () => {
   const your_pwd = screen.getByText("password");
   expect(your_pwd).toBeInTheDocument();
   const log_in = screen.getByText("Log In");
-  expect(log_in).toBeInTheDocument();
-  
+  expect(log_in).toBeInTheDocument();  
 
   
   
