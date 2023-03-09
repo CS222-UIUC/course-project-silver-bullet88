578eb786fb65be8857c03deb2b770178ac9dde67
Ding-jinyu <jinyud2@illinois.edu>
2023-03-08 18:19:03 -0600

    comment out UsersHome.js to make sure website is able to run

--- Project/src/pages/UsersHome.js
+++ Project/src/pages/UsersHome.js
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-const UsersHome = () => {
-  const user = {
-    firstName: 'Madivh',
-    lastName: 'Kassel',
-    phonenumber: 0000000000,
-    Gender: "none",
-    email: 'zhuoang2@iilinois.edu',
-    age: 20,
-    college: 'UIUC',
-    bio: 'I am a computer sciecne student in UIUC. I love to write code and solve problems.',
-    interests: ['Programming', 'Gaming', 'Soccer'],
-  };
+// const UsersHome = () => {
+//   const user = {
+//     firstName: 'Madivh',
+//     lastName: 'Kassel',
+//     phonenumber: 0000000000,
+//     Gender: "none",
+//     email: 'zhuoang2@iilinois.edu',
+//     age: 20,
+//     college: 'UIUC',
+//     bio: 'I am a computer sciecne student in UIUC. I love to write code and solve problems.',
+//     interests: ['Programming', 'Gaming', 'Soccer'],
+//   };
 
-  return (
-    <div>
-      <h1>{user.firstName}'s Homepage</h1>
-      <p>Name: {user.firstName} {user.lastName}</p>
-      <p>Email: {user.email}</p>
-      <p>Age: {user.age}</p>
-      <p>college: {user.college}</p>
-      <p>Bio: {user.bio}</p>
-      <p>Interests: {user.interests.join(', ')}</p>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>{user.firstName}'s Homepage</h1>
+//       <p>Name: {user.firstName} {user.lastName}</p>
+//       <p>Email: {user.email}</p>
+//       <p>Age: {user.age}</p>
+//       <p>college: {user.college}</p>
+//       <p>Bio: {user.bio}</p>
+//       <p>Interests: {user.interests.join(', ')}</p>
+//     </div>
+//   );
+// };
   
-export default UsersHome;
+// export default UsersHome;
