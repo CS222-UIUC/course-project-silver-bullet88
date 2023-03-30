32bb8158fe79d443f30b239e92e61ca72f1bd23d
Ding-jinyu <jinyud2@illinois.edu>
2023-03-29 19:11:52 -0500

    created database to store user info and reject to login if pwd or email didn't match the info in database

--- Project/src/pages/login.js
+++ Project/src/pages/login.js
@@ -1,19 +1,50 @@
 import React, { useState } from "react";
 import {NavLink} 
     from "../components/Navbar/NavbarElements";
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import {db} from '../firebase';
 // I'll work on the signup page (Jinyu Ding)
 // !!!!!!! Pls don't edit this file <3
 const Login = () => {
   // track state
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = (e) => {
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // print email
     console.log(email);
+
+    const userRef = collection(db, "Users");
+    const q = query(userRef, where("email", "==", email));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      if(doc.data().password === password) {
+        console.log("success")
+        setError(0);
+      } else {
+        console.log("failure")
+        setError(1);
+      }
+    });
+    
+    
   }
+  // error func
+  const errorMessage = () => {
+    return (
+    <div
+        className="error"
+        style={{
+        display: error===1 ? '' : 'none',
+        }}>
+        <h4>Wrong pwd or email. Please enter again!</h4>
+    </div>
+    );
+  };
   return (
     <div className= "login">
       <form className = "login-form" onSubmit={handleSubmit}>
@@ -29,6 +60,8 @@ const Login = () => {
       <NavLink className="switch-link" to="/register">
       Don't have an account? Register here!
       </NavLink>
+      {/* print error message */}
+      <div className="messages"> {errorMessage()} </div>
     </div>
     // </div>
   );
