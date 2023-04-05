d76baf2036f66745d8963ca88e10b5d6abd80401
Ding-jinyu <jinyud2@illinois.edu>
2023-04-05 17:07:28 -0500

    linked login button with user page iff user entered correct email & pwd. Added QB link on user page.

--- Project/src/pages/UsersHome/UsersHome.js
+++ Project/src/pages/UsersHome/UsersHome.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./UsersHome.css";
 import FlashcardList from "./FlashcardList";
+import {NavLink} 
+    from "../../components/Navbar/NavbarElements";
 
 const UsersHome = ({flashcards}) => {
     return (
       <div className="container">
         <FlashcardList flashcards={flashcards}></FlashcardList>
+        <h2>⬇️ Here is ur Question box link! Share it with ur friends 💗</h2>
+        <NavLink className="switch-link" to="/question">
+        click here
+        </NavLink>
+        
       </div>
     )
 };
