3b02b7d3b5a8661d2ffb7b58834c8e23e9b88f42
Qi Wu <wuqi1126@126.com>
2023-04-05 16:21:17 -0500

    add answer field in firebase

--- Project/src/pages/Question.js
+++ Project/src/pages/Question.js
@@ -16,7 +16,8 @@ const Question = () => {
     
     console.log(question);
     try {
-      const docRef = addDoc(collection(db, "Questions"), {question});
+      var answer = "";
+      const docRef = addDoc(collection(db, "Questions"), {question,answer});
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
