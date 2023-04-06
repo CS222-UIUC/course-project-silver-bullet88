72d2bea94d1655765693833d01711607263d77c3
Fr4nk03 <jx37@illinois.edu>
2023-04-06 17:11:05 -0500

    add popup and buttons
    still has bugs

--- Project/src/components/Navbar/Navbar.js
+++ Project/src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-            <NavLink to="/" className="navbar-logo">
+            <NavLink to="/home" className="navbar-logo">
                 AskMe <i className='fab.fa-typo3' />   
             </NavLink>
             <div className='menu-icon'>
