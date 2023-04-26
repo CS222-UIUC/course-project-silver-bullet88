e6b401b345f25dda26c8da327939051bbc109407
Fr4nk03 <jx37@illinois.edu>
2023-04-26 16:13:38 -0500

    emoji!

--- Project/src/pages/ContactUs.js
+++ Project/src/pages/ContactUs.js
@@ -5,7 +5,7 @@ const Contact = () => {
       <h1>Any suggestions about our website? Feel free to reach out! 💗</h1>
       <h3>Developers: </h3>
       <h3>Qi 👨‍🔬: wuqi1126@126.com </h3>
-      <h3>Frank 🧔🏿‍♂️: jx37@illinois.edu</h3>
+      <h3>Frank ⛹🏻: jx37@illinois.edu</h3>
       <h3>Tina 👩🏻‍💻: jinyud2@illinois.edu</h3>
       Acknowledgment: Our mentor Trisha 💟
     </div>
